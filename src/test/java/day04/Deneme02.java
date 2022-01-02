08f5063778828d1688e2b2b28d61004aec33683e
Mustafa Gecer <h.musti.27@gmail.com>
2022-01-02 07:59:07 +0100

    day08

---
+++ src/test/java/day04/Deneme02.java
@@ -0,0 +1,24 @@
+package day04;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Deneme02 {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get("https://www.softwaretestinghelp.com/");
+
+
+
+        Thread.sleep(5000);
+        driver.close();
+    }
+}
--- src/test/java/day05/Exercise02.java
+++ src/test/java/day05/Exercise02.java
@@ -1,4 +1,51 @@
 package day05;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Exercise02 {
+
+     static WebDriver driver;
+
+
+    @BeforeClass
+    public static void setup() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void shopping(){
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.xpath("(//*[@class='inventory_item_name'])[3]")).click();
+        System.out.println(driver.findElement(By.xpath("//*[@class='inventory_details_price']")).getText());
+
+        WebElement alianacakUrun= driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
+        alianacakUrun.click();
+
+
+
+    }
+
+    @AfterClass
+    public static void close(){
+        driver.close();
+
+    }
+
 }
